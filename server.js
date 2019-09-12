28fc4bd687115c779bcf0ee9183b9460e4bdb131
Martin Hartman <hartmanx1@wp.pl>
2019-09-12 04:44:08 +0200

    29done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const User = mongoose.model('User', userSchema);
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://dbUser:wY!:35x-GNnirM7@cluster0-vcbxk.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://hartmanx1:mongo123@cluster0-gpmit.mongodb.net/test?retryWrites=true&w=majority', {
     useMongoClient: true
 });
 
